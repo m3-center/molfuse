549b99f24194c3e57c3c2733b6c931e6b2cbe09e
Alexander Hagg <info@haggdesign.de>
2025-06-11 12:19:32 +0200

    Added 2 days to hpc script

--- hpc/ummbas_leaveoneout.sh
+++ hpc/ummbas_leaveoneout.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1                # number of nodes
 #SBATCH --ntasks-per-node=32     # number of tasks per node
 #SBATCH --mem=400G               # memory per node in MB (different units with suffix K|M|G|T)
-#SBATCH --time=24:00:00              # total runtime of job allocation (format D-HH:MM:SS; first parts optional)
+#SBATCH --time=3-00:00:00              # total runtime of job allocation (format D-HH:MM:SS; first parts optional)
 #SBATCH --output=slurm.%j.out    # filename for STDOUT (%N: nodename, %j: job-ID)
 #SBATCH --error=slurm.%j.err     # filename for STDERR
 #SBATCH --gres=gpu:1
